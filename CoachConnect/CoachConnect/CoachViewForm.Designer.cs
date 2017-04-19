437295582def5e4cc2add855f62df2e2133cd456
TDurk8 <tim.durkee@gmail.com>
2017-04-18 21:47:31 -0500

    Edit Coach's profile updated

--- CoachConnect/CoachConnect/CoachViewForm.Designer.cs
+++ CoachConnect/CoachConnect/CoachViewForm.Designer.cs
@@ -31,16 +31,25 @@
             this.tbForm = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.pbProfile = new System.Windows.Forms.PictureBox();
-            this.lblCoachUser = new System.Windows.Forms.Label();
-            this.lblCoachName = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.dgvAvailable = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCoachName = new System.Windows.Forms.Label();
+            this.lblCoachUser = new System.Windows.Forms.Label();
+            this.pbProfile = new System.Windows.Forms.PictureBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.pbEditPic = new System.Windows.Forms.PictureBox();
+            this.btnEditPic = new System.Windows.Forms.Button();
+            this.txtPicURL = new System.Windows.Forms.TextBox();
+            this.btnPreview = new System.Windows.Forms.Button();
+            this.btnPicSubmit = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
             this.tbForm.SuspendLayout();
             this.tabPage2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pbProfile)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvAvailable)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbProfile)).BeginInit();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEditPic)).BeginInit();
             this.SuspendLayout();
             // 
             // tbForm
@@ -48,6 +57,7 @@
             this.tbForm.Controls.Add(this.tabPage1);
             this.tbForm.Controls.Add(this.tabPage2);
             this.tbForm.Controls.Add(this.tabPage3);
+            this.tbForm.Controls.Add(this.tabPage4);
             this.tbForm.Location = new System.Drawing.Point(12, 12);
             this.tbForm.Name = "tbForm";
             this.tbForm.SelectedIndex = 0;
@@ -79,43 +89,13 @@
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Current Schedule";
             // 
-            // tabPage3
+            // dgvAvailable
             // 
-            this.tabPage3.BackColor = System.Drawing.SystemColors.Control;
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(530, 421);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "Edit Availablity";
-            // 
-            // pbProfile
-            // 
-            this.pbProfile.Location = new System.Drawing.Point(20, 20);
-            this.pbProfile.Name = "pbProfile";
-            this.pbProfile.Size = new System.Drawing.Size(100, 100);
-            this.pbProfile.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pbProfile.TabIndex = 0;
-            this.pbProfile.TabStop = false;
-            // 
-            // lblCoachUser
-            // 
-            this.lblCoachUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCoachUser.Location = new System.Drawing.Point(126, 52);
-            this.lblCoachUser.Name = "lblCoachUser";
-            this.lblCoachUser.Size = new System.Drawing.Size(398, 23);
-            this.lblCoachUser.TabIndex = 1;
-            this.lblCoachUser.Text = "username";
-            this.lblCoachUser.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // lblCoachName
-            // 
-            this.lblCoachName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCoachName.Location = new System.Drawing.Point(126, 20);
-            this.lblCoachName.Name = "lblCoachName";
-            this.lblCoachName.Size = new System.Drawing.Size(398, 23);
-            this.lblCoachName.TabIndex = 2;
-            this.lblCoachName.Text = "coach";
-            this.lblCoachName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.dgvAvailable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAvailable.Location = new System.Drawing.Point(6, 209);
+            this.dgvAvailable.Name = "dgvAvailable";
+            this.dgvAvailable.Size = new System.Drawing.Size(518, 197);
+            this.dgvAvailable.TabIndex = 5;
             // 
             // label1
             // 
@@ -127,13 +107,120 @@
             this.label1.TabIndex = 4;
             this.label1.Text = "Current Availability";
             // 
-            // dgvAvailable
+            // lblCoachName
             // 
-            this.dgvAvailable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvAvailable.Location = new System.Drawing.Point(6, 209);
-            this.dgvAvailable.Name = "dgvAvailable";
-            this.dgvAvailable.Size = new System.Drawing.Size(518, 197);
-            this.dgvAvailable.TabIndex = 5;
+            this.lblCoachName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCoachName.Location = new System.Drawing.Point(126, 20);
+            this.lblCoachName.Name = "lblCoachName";
+            this.lblCoachName.Size = new System.Drawing.Size(398, 23);
+            this.lblCoachName.TabIndex = 2;
+            this.lblCoachName.Text = "coach";
+            this.lblCoachName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCoachUser
+            // 
+            this.lblCoachUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCoachUser.Location = new System.Drawing.Point(126, 52);
+            this.lblCoachUser.Name = "lblCoachUser";
+            this.lblCoachUser.Size = new System.Drawing.Size(398, 23);
+            this.lblCoachUser.TabIndex = 1;
+            this.lblCoachUser.Text = "username";
+            this.lblCoachUser.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pbProfile
+            // 
+            this.pbProfile.Location = new System.Drawing.Point(20, 20);
+            this.pbProfile.Name = "pbProfile";
+            this.pbProfile.Size = new System.Drawing.Size(100, 100);
+            this.pbProfile.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbProfile.TabIndex = 0;
+            this.pbProfile.TabStop = false;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(530, 421);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Edit Availablity";
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage4.Controls.Add(this.btnCancel);
+            this.tabPage4.Controls.Add(this.btnPicSubmit);
+            this.tabPage4.Controls.Add(this.btnPreview);
+            this.tabPage4.Controls.Add(this.txtPicURL);
+            this.tabPage4.Controls.Add(this.btnEditPic);
+            this.tabPage4.Controls.Add(this.pbEditPic);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(530, 421);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Edit Profile";
+            // 
+            // pbEditPic
+            // 
+            this.pbEditPic.Location = new System.Drawing.Point(26, 30);
+            this.pbEditPic.Name = "pbEditPic";
+            this.pbEditPic.Size = new System.Drawing.Size(150, 150);
+            this.pbEditPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbEditPic.TabIndex = 1;
+            this.pbEditPic.TabStop = false;
+            // 
+            // btnEditPic
+            // 
+            this.btnEditPic.Location = new System.Drawing.Point(182, 30);
+            this.btnEditPic.Name = "btnEditPic";
+            this.btnEditPic.Size = new System.Drawing.Size(75, 23);
+            this.btnEditPic.TabIndex = 2;
+            this.btnEditPic.Text = "Edit Pic";
+            this.btnEditPic.UseVisualStyleBackColor = true;
+            this.btnEditPic.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtPicURL
+            // 
+            this.txtPicURL.Enabled = false;
+            this.txtPicURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPicURL.Location = new System.Drawing.Point(182, 59);
+            this.txtPicURL.Name = "txtPicURL";
+            this.txtPicURL.Size = new System.Drawing.Size(342, 26);
+            this.txtPicURL.TabIndex = 3;
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.Location = new System.Drawing.Point(182, 30);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(75, 23);
+            this.btnPreview.TabIndex = 4;
+            this.btnPreview.Text = "Preview";
+            this.btnPreview.UseVisualStyleBackColor = true;
+            this.btnPreview.Visible = false;
+            this.btnPreview.Click += new System.EventHandler(this.btnPreview_Click);
+            // 
+            // btnPicSubmit
+            // 
+            this.btnPicSubmit.Location = new System.Drawing.Point(182, 30);
+            this.btnPicSubmit.Name = "btnPicSubmit";
+            this.btnPicSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnPicSubmit.TabIndex = 5;
+            this.btnPicSubmit.Text = "Submit";
+            this.btnPicSubmit.UseVisualStyleBackColor = true;
+            this.btnPicSubmit.Visible = false;
+            this.btnPicSubmit.Click += new System.EventHandler(this.btnPicSubmit_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(263, 30);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Visible = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // CoachViewForm
             // 
@@ -149,8 +236,11 @@
             this.tbForm.ResumeLayout(false);
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pbProfile)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvAvailable)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbProfile)).EndInit();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEditPic)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -166,5 +256,12 @@
         private System.Windows.Forms.PictureBox pbProfile;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dgvAvailable;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.PictureBox pbEditPic;
+        private System.Windows.Forms.Button btnEditPic;
+        private System.Windows.Forms.TextBox txtPicURL;
+        private System.Windows.Forms.Button btnPreview;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnPicSubmit;
     }
 }
