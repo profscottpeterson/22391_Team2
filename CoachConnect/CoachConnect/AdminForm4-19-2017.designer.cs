4f91819d3676e09ca4fcad503dbea30549d016f4
Adam Smith <radarsmith83@gmail.com>
2017-04-19 16:24:59 -0500

    Adding Admin form manually from Brian's flash drive...he is having trouble syncing with master so we'll try a different method.

---
+++ CoachConnect/CoachConnect/AdminForm4-19-2017.designer.cs
@@ -0,0 +1,326 @@
+﻿namespace CoachConnect
+{
+    partial class AdminForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAdminForm = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtBoxUserID = new System.Windows.Forms.TextBox();
+            this.lblUserID = new System.Windows.Forms.Label();
+            this.lblMiddleName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.txtBoxMiddleName = new System.Windows.Forms.TextBox();
+            this.txtBoxPassword = new System.Windows.Forms.TextBox();
+            this.txtBoxLastName = new System.Windows.Forms.TextBox();
+            this.txtBoxFirstName = new System.Windows.Forms.TextBox();
+            this.lstBoxUsers = new System.Windows.Forms.ListBox();
+            this.lblUsers = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnLogOff = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.chkBoxStudent = new System.Windows.Forms.CheckBox();
+            this.chkBoxCoach = new System.Windows.Forms.CheckBox();
+            this.chkBoxAdmin = new System.Windows.Forms.CheckBox();
+            this.chkBoxActive = new System.Windows.Forms.CheckBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblAdminForm
+            // 
+            this.lblAdminForm.AutoSize = true;
+            this.lblAdminForm.Font = new System.Drawing.Font("Baskerville Old Face", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAdminForm.Location = new System.Drawing.Point(200, 9);
+            this.lblAdminForm.Name = "lblAdminForm";
+            this.lblAdminForm.Size = new System.Drawing.Size(367, 73);
+            this.lblAdminForm.TabIndex = 7;
+            this.lblAdminForm.Text = "Admin Form";
+            this.lblAdminForm.UseWaitCursor = true;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(441, 372);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(103, 25);
+            this.lblPassword.TabIndex = 43;
+            this.lblPassword.Text = " Password";
+            // 
+            // txtBoxUserID
+            // 
+            this.txtBoxUserID.Location = new System.Drawing.Point(448, 336);
+            this.txtBoxUserID.Name = "txtBoxUserID";
+            this.txtBoxUserID.Size = new System.Drawing.Size(188, 33);
+            this.txtBoxUserID.TabIndex = 42;
+            // 
+            // lblUserID
+            // 
+            this.lblUserID.AutoSize = true;
+            this.lblUserID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserID.Location = new System.Drawing.Point(443, 308);
+            this.lblUserID.Name = "lblUserID";
+            this.lblUserID.Size = new System.Drawing.Size(77, 25);
+            this.lblUserID.TabIndex = 40;
+            this.lblUserID.Text = "User ID";
+            // 
+            // lblMiddleName
+            // 
+            this.lblMiddleName.AutoSize = true;
+            this.lblMiddleName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMiddleName.Location = new System.Drawing.Point(443, 244);
+            this.lblMiddleName.Name = "lblMiddleName";
+            this.lblMiddleName.Size = new System.Drawing.Size(127, 25);
+            this.lblMiddleName.TabIndex = 39;
+            this.lblMiddleName.Text = "Middle Name";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastName.Location = new System.Drawing.Point(443, 180);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(106, 25);
+            this.lblLastName.TabIndex = 38;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirstName.Location = new System.Drawing.Point(443, 116);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(106, 25);
+            this.lblFirstName.TabIndex = 37;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // txtBoxMiddleName
+            // 
+            this.txtBoxMiddleName.Location = new System.Drawing.Point(448, 272);
+            this.txtBoxMiddleName.Name = "txtBoxMiddleName";
+            this.txtBoxMiddleName.Size = new System.Drawing.Size(188, 33);
+            this.txtBoxMiddleName.TabIndex = 36;
+            // 
+            // txtBoxPassword
+            // 
+            this.txtBoxPassword.Location = new System.Drawing.Point(448, 400);
+            this.txtBoxPassword.Name = "txtBoxPassword";
+            this.txtBoxPassword.Size = new System.Drawing.Size(188, 33);
+            this.txtBoxPassword.TabIndex = 35;
+            // 
+            // txtBoxLastName
+            // 
+            this.txtBoxLastName.Location = new System.Drawing.Point(448, 208);
+            this.txtBoxLastName.Name = "txtBoxLastName";
+            this.txtBoxLastName.Size = new System.Drawing.Size(188, 33);
+            this.txtBoxLastName.TabIndex = 34;
+            // 
+            // txtBoxFirstName
+            // 
+            this.txtBoxFirstName.Location = new System.Drawing.Point(448, 144);
+            this.txtBoxFirstName.Name = "txtBoxFirstName";
+            this.txtBoxFirstName.Size = new System.Drawing.Size(188, 33);
+            this.txtBoxFirstName.TabIndex = 33;
+            // 
+            // lstBoxUsers
+            // 
+            this.lstBoxUsers.Font = new System.Drawing.Font("Microsoft NeoGothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstBoxUsers.FormattingEnabled = true;
+            this.lstBoxUsers.ItemHeight = 27;
+            this.lstBoxUsers.Location = new System.Drawing.Point(12, 125);
+            this.lstBoxUsers.Name = "lstBoxUsers";
+            this.lstBoxUsers.Size = new System.Drawing.Size(269, 490);
+            this.lstBoxUsers.TabIndex = 47;
+            // 
+            // lblUsers
+            // 
+            this.lblUsers.AutoSize = true;
+            this.lblUsers.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsers.Location = new System.Drawing.Point(83, 82);
+            this.lblUsers.Name = "lblUsers";
+            this.lblUsers.Size = new System.Drawing.Size(105, 41);
+            this.lblUsers.TabIndex = 48;
+            this.lblUsers.Text = "Users";
+            this.lblUsers.Click += new System.EventHandler(this.lblUsers_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(352, 545);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(97, 47);
+            this.btnSubmit.TabIndex = 49;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // btnLogOff
+            // 
+            this.btnLogOff.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogOff.Location = new System.Drawing.Point(490, 545);
+            this.btnLogOff.Name = "btnLogOff";
+            this.btnLogOff.Size = new System.Drawing.Size(99, 47);
+            this.btnLogOff.TabIndex = 50;
+            this.btnLogOff.Text = "Log Off";
+            this.btnLogOff.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(635, 545);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(99, 47);
+            this.btnExit.TabIndex = 51;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // chkBoxStudent
+            // 
+            this.chkBoxStudent.AutoSize = true;
+            this.chkBoxStudent.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkBoxStudent.Location = new System.Drawing.Point(664, 213);
+            this.chkBoxStudent.Name = "chkBoxStudent";
+            this.chkBoxStudent.Size = new System.Drawing.Size(106, 29);
+            this.chkBoxStudent.TabIndex = 52;
+            this.chkBoxStudent.Text = "Student";
+            this.chkBoxStudent.UseVisualStyleBackColor = true;
+            // 
+            // chkBoxCoach
+            // 
+            this.chkBoxCoach.AutoSize = true;
+            this.chkBoxCoach.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkBoxCoach.Location = new System.Drawing.Point(664, 248);
+            this.chkBoxCoach.Name = "chkBoxCoach";
+            this.chkBoxCoach.Size = new System.Drawing.Size(96, 29);
+            this.chkBoxCoach.TabIndex = 53;
+            this.chkBoxCoach.Text = "Coach";
+            this.chkBoxCoach.UseVisualStyleBackColor = true;
+            // 
+            // chkBoxAdmin
+            // 
+            this.chkBoxAdmin.AutoSize = true;
+            this.chkBoxAdmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkBoxAdmin.Location = new System.Drawing.Point(664, 178);
+            this.chkBoxAdmin.Name = "chkBoxAdmin";
+            this.chkBoxAdmin.Size = new System.Drawing.Size(94, 29);
+            this.chkBoxAdmin.TabIndex = 54;
+            this.chkBoxAdmin.Text = "Admin";
+            this.chkBoxAdmin.UseVisualStyleBackColor = true;
+            // 
+            // chkBoxActive
+            // 
+            this.chkBoxActive.AutoSize = true;
+            this.chkBoxActive.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkBoxActive.Location = new System.Drawing.Point(664, 143);
+            this.chkBoxActive.Name = "chkBoxActive";
+            this.chkBoxActive.Size = new System.Drawing.Size(92, 29);
+            this.chkBoxActive.TabIndex = 55;
+            this.chkBoxActive.Text = "Active";
+            this.chkBoxActive.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(12, 569);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(135, 46);
+            this.btnAdd.TabIndex = 56;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinus.Location = new System.Drawing.Point(144, 569);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(137, 46);
+            this.btnMinus.TabIndex = 57;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            // 
+            // AdminForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 627);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.chkBoxActive);
+            this.Controls.Add(this.chkBoxAdmin);
+            this.Controls.Add(this.chkBoxCoach);
+            this.Controls.Add(this.chkBoxStudent);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLogOff);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lblUsers);
+            this.Controls.Add(this.lstBoxUsers);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtBoxUserID);
+            this.Controls.Add(this.lblUserID);
+            this.Controls.Add(this.lblMiddleName);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.txtBoxMiddleName);
+            this.Controls.Add(this.txtBoxPassword);
+            this.Controls.Add(this.txtBoxLastName);
+            this.Controls.Add(this.txtBoxFirstName);
+            this.Controls.Add(this.lblAdminForm);
+            this.Font = new System.Drawing.Font("MV Boli", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "AdminForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblAdminForm;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtBoxUserID;
+        private System.Windows.Forms.Label lblUserID;
+        private System.Windows.Forms.Label lblMiddleName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.TextBox txtBoxMiddleName;
+        private System.Windows.Forms.TextBox txtBoxPassword;
+        private System.Windows.Forms.TextBox txtBoxLastName;
+        private System.Windows.Forms.TextBox txtBoxFirstName;
+        private System.Windows.Forms.ListBox lstBoxUsers;
+        private System.Windows.Forms.Label lblUsers;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnLogOff;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.CheckBox chkBoxStudent;
+        private System.Windows.Forms.CheckBox chkBoxCoach;
+        private System.Windows.Forms.CheckBox chkBoxAdmin;
+        private System.Windows.Forms.CheckBox chkBoxActive;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnMinus;
+    }
+}
+
